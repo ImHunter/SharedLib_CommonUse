d62d1e6a7ea147164e5d82fe4660d47f34e89db4
ImHunter <dolf@land.ru>
2018-02-16 16:15:14 +0500

    Реализация функционала класса

--- src/Energos/Jenkins/Common/Dir.groovy
+++ src/Energos/Jenkins/Common/Dir.groovy
@@ -3,7 +3,26 @@ package Energos.Jenkins.Common
 import java.io.File
 
 class Dir extends File {
+
     Dir(String s) {
         super(s)
     }
+
+    boolean hasFiles(String mask = null, Date minModifyDate = null){
+        File[] files = findFiles(mask, minModifyDate)
+        files.length>0
+    }
+
+    File[] findFiles(String mask = null, Date minModifyDate = null){
+        ArrayList<File> lst = new ArrayList()
+        eachFileMatch(mask==null ? '*.*' : mask) {File f ->
+            if (f.isFile()) {
+                if (minModifyDate!=null && (Date(f.lastModified())>=minModifyDate)) {
+                    lst.add(f)
+                }
+            }
+        }
+        lst.toArray()
+    }
+
 }
