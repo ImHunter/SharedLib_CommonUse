e8d17d8f1376e29a9411fd53404adf4ff62f3b2f
ImHunter <dolf@land.ru>
2018-02-17 10:53:32 +0500

    Делаем поиск файлов

--- src/Energos/Jenkins/Common/Dir.groovy
+++ src/Energos/Jenkins/Common/Dir.groovy
@@ -8,15 +8,55 @@ class Dir extends File {
         super(s)
     }
 
+    static String wildcardToRegex(String wildcard){
+        StringBuffer s = new StringBuffer(wildcard.length())
+        s.append('^')
+        is = wildcard.length()
+        for (int i = 0; i < is; i++) {
+            char c = wildcard.charAt(i)
+            switch(c) {
+                case '*':
+                    s.append(".*")
+                    break
+                case '?':
+                    s.append(".")
+                    break
+                case '^': // escape character in cmd.exe
+                    s.append("\\")
+                    break
+            // escape special regexp-characters
+                case '(': case ')': case '[': case ']': case '$':
+                case '.': case '{': case '}': case '|':
+                case '\\':
+                    s.append("\\")
+                    s.append(c)
+                    break
+                default:
+                    s.append(c)
+                    break
+            }
+        }
+        s.append('$')
+        s.toString()
+    }
+
     boolean hasFiles(String mask = null, Date minModifyDate = null){
         File[] files = findFiles(mask, minModifyDate)
         files.length>0
     }
 
     File[] findFiles(String mask = null, Date minModifyDate = null){
+
         ArrayList<File> lst = new ArrayList()
         boolean dateChecked
-        eachFileMatch(mask==null ? '*.*' : mask) {File f ->
+
+//        DirectoryScanner scanner = new DirectoryScanner()
+//        scanner.setIncludes([mask==null ? '*.*' : mask])
+//        scanner.setBasedir(this.)
+//        scanner.setCaseSensitive(false)
+
+        String patt = wildcardToRegex(mask==null ? '*.*' : mask)
+        eachFileMatch(patt) {File f ->
             if (f.isFile()) {
                 dateChecked = minModifyDate==null || (minModifyDate!=null && (new Date(f.lastModified())>=minModifyDate))
                 if (dateChecked)
