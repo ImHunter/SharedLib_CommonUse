14ff1d5eccfb33ea5c373bedabaca860fe029928
ImHunter <imhunter74@gmail.com>
2019-04-18 09:22:00 +0500

    добавил эхо при чтении ключа

--- vars/envVar.groovy
+++ vars/envVar.groovy
@@ -1,13 +1,15 @@
 /**
  * Возвращает значение ключа из переменных среды
  * @param envKey - Ключ (строка)
- * @param withAssert - Требовать наличие ключа и его значения (Булево)
+ * @param required - Требовать наличие ключа и его значения (Булево)
  * @return - Значение ключа
  */
-def call(def envKey, def withAssert = true){
+def call(def envKey, def required = true){
 
     def res = env[envKey]
-    if (withAssert==true)
+    if (res==null)
+        echo("Ключ $envKey отсутствует или его значение не задано")
+    if (required==true)
         assert res != null
 
     res
